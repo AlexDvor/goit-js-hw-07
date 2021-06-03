a7376f968b662aa11a6cc0e962854ca01e53b129
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-06-03 21:52:42 +0200

    finished 7 exr

---
+++ js/task-07.js
@@ -0,0 +1,12 @@
+const fontSizeControl = document.querySelector('#font-size-control');
+const textSize = document.querySelector('#text');
+
+
+fontSizeControl.addEventListener('click',onInputClick);
+
+function onInputClick(event) {
+    const rangeValue = event.currentTarget.value;
+    const changeSize = textSize.style.fontSize = rangeValue + 'px';
+    console.log(`Размер данного шрифта ровно ${changeSize}`);
+    
+}
